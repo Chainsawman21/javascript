381baffb8f9d5a51649092bda2cf3f947935bd69
Ackerman <666@.ru>
2022-12-22 17:47:08 +0500

    save

---
+++ main.js
@@ -0,0 +1,20 @@
+var ackerman ='Levi'
+ackerman ='зовут '
+var test = 'Mikasa'
+var age = 20;
+
+var Levi = prompt('Ведите свое имя');
+console.log('Меня '  + ackerman  + Levi );
+var kenny = prompt('Укажите свой возраст:');
+console.log('Мне ' + kenny );
+var eren = prompt('Решите пример 6+3=?');
+console.log('Пример 1: 6 + 3 = 9 Ваш ответ:' + eren ) ;
+var ervin = prompt('Решите пример 20-5=?');
+console.log('Пример 2: 6 + 3 = 9 Ваш ответ:' + ervin ) ;
+var armin = prompt('Решите пример 15*2=?');
+console.log('Пример 3: 15 * 2 = 30 Ваш ответ:' + armin ) ;
+var zeck = prompt('Решите пример 8/4=2?');
+console.log('Пример 4: 8 / 4 = 2  Ваш ответ:' + zeck ) ;
+var rainer = prompt('Решите пример 10%3=1?');
+console.log('Пример 5: 10 % 3 = 1  Ваш ответ:' + rainer ) ;
+alert('Откройте консоль для просмотра');
